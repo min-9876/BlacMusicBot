e01456f5c8c44129e19b5e60acb325c708d06128
min-9876 <musicmin2029@gmail.com>
2026-05-27 13:11:23 +0630

    Update youtube.py

--- BlacMusic/core/youtube.py
+++ BlacMusic/core/youtube.py
@@ -22,7 +22,7 @@ from pathlib import Path
 from typing import Optional, Union
 
 from pyrogram import enums, types
-from py_yt import Playlist, VideosSearch
+from youtube_search_python import Playlist, VideosSearch
 from BlacMusic import config, logger
 from BlacMusic.helpers import Track, utils
 
